87838032251daadca7a977ae081053d647f3158c
prateek-srivastava001 <prateeksrivastava201@gmail.com>
2024-09-21 16:48:26 +0530

    fix

--- internal/services/s3_service.go
+++ internal/services/s3_service.go
@@ -4,6 +4,12 @@ import (
 	"EasySplit/internal/database"
 )
 
+func StoreMediaURL(email, imageURL, audioURL string) error {
+	query := `INSERT INTO user_media (user_email, image_url, audio_url) VALUES ($1, $2, $3)`
+	_, err := database.DB.Exec(query, email, imageURL, audioURL)
+	return err
+}
+
 func GetImageURL(email string) (string, error) {
 	var imageURL string
 	query := `SELECT image_url FROM user_media WHERE user_email = $1`
